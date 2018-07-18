420a4b2741df46bc06ec9da332ff29846b3b3dbe
Stéphane Zucatti <stephane.zucatti@me.com>
2018-07-18 10:33:38 +0200

    UPD: support ADFS passport

--- Contents/bin/lib/server/auth.js
+++ Contents/bin/lib/server/auth.js
@@ -1,10 +1,11 @@
 module.exports = function (app) {
 
-    var util = require('../util');
+    var authom = require("@omneedia/authom");
+    var fs = require('fs');
+    var path = require('path');
     var sep = "/";
-    var passport = require('passport');
-    app.use(passport.initialize());
-    app.use(passport.session());
+
+    var util = require('../util');
 
     global.Auth = {
         officer: function (req, profile, fn) {
@@ -30,7 +31,6 @@ module.exports = function (app) {
             Officer.getProfile = function (user, cb) {
                 var response = [];
                 if (cb) {
-
                     fs.readFile(global.PROJECT_AUTH + sep + 'Profiler.json', function (e, r) {
                         var profiler = JSON.parse(r.toString('utf-8'));
                         for (var el in profiler.profile) {
@@ -53,7 +53,6 @@ module.exports = function (app) {
                 }
             };
             Officer.login(profile, function (err, response) {
-                console.log(err);
                 if (err) return cb(err);
                 req.session.authType = auth_type;
                 req.session.user = response;
@@ -62,13 +61,76 @@ module.exports = function (app) {
         }
     };
 
-    function onAuthenticate(req, res) {
-        var profile = {
-            service: req.params.service,
-            username: req.user
-        };
-        //console.log(profile);
+    app.get('/bye', function (req, res) {
+        res.setHeader('content-type', 'text/html');
+        res.end('<script>top.location.reload(true);window.close();</script>');
+    });
+
+    app.post('/pid', function (req, res) {
+        var authType = req.session.authType;
+        req.session.destroy();
+        if (global.settings.auth[authType.toLowerCase()]) var url = global.settings.auth[authType.toLowerCase()].logout;
+        else var url = "/bye";
+        return res.redirect(url);
+    });
+
+    app.get('/logout', function (req, res) {
+        console.log(req.session);
+        var authType = req.session.authType;
+        req.session.destroy();
+        if (global.settings.auth[authType.toLowerCase()]) var url = global.settings.auth[authType.toLowerCase()].logout;
+        else var url = "/bye";
+        return res.redirect(url);
+    });
+
+    function ensureAuthenticated(req, res, next) {
+        if (!req.user) req.user = req.session.user;
+        if (req.user) return next();
+        res.end('{"response":"NOT_LOGIN"}');
+    };
+
+    app.get('/account', ensureAuthenticated, function (req, res) {
+        if (!req.user) req.user = req.session.user;
+        var response = [];
+        fs.readFile(global.PROJECT_AUTH + sep + 'Profiler.json', function (e, r) {
+            if (e) return res.end(JSON.stringify(req.user));
+            var profiler = JSON.parse(r.toString('utf-8'));
+            for (var el in profiler.profile) {
+                var p = profiler.profile[el];
+                if (p.indexOf(req.user.mail.split('@')[0]) > -1) response.push(el);
+            };
+            req.user.profiles = response;
+            res.end(JSON.stringify(req.user));
+        });
+    });
+
+    app.post('/account', ensureAuthenticated, function (req, res) {
+
+        if (!req.user) req.user = req.session.user;
+        var response = [];
+        fs.readFile(global.PROJECT_AUTH + sep + 'Profiler.json', function (e, r) {
+            if (e) return res.end(JSON.stringify(req.user));
+            var profiler = JSON.parse(r.toString('utf-8'));
+            for (var el in profiler.profile) {
+                var p = profiler.profile[el];
+                if (p.indexOf(req.user.mail.split('@')[0]) > -1) response.push(el);
+            };
+            req.user.profiles = response;
+            res.end(JSON.stringify(req.user));
+        });
+
+    });
+
+    for (var el in global.settings.auth) {
+        var o = global.settings.auth[el];
+        o.service = o.type;
+        authom.createServer(o);
+    };
+
+    authom.on("auth", function (req, res, data) {
+        profile = data;
         Auth.officer(req, profile, function (err, response) {
+
             if (!response) {
                 global.OASocketonFailedAuth(response);
                 // Close the login window
@@ -81,99 +143,20 @@ module.exports = function (app) {
                 req.session.user = response;
                 global.OASocketonAuth(JSON.stringify(response));
             } catch (e) {
-                console.log(e);
                 global.OASocketonFailedAuth(response);
             }
-            global.request({
-                uri: global.Config.host + '/profile',
-                method: "POST",
-                form: {
-                    task: global.Config.task,
-                    user: req.session.user.mail.toLowerCase()
-                }
-            }, function (e, r, bx) {
-                var profiler = JSON.parse(bx);
-                var rp = [];
-                for (var i = 0; i < profiler.length; i++) {
-                    rp.push(profiler[i].name);
-                };
-                req.user.profiles = rp;
-                // Close the login window
-                res.set('Content-Type', 'text/html');
-                res.end("<html><body><script>setTimeout(window.close, 100);</script></body></html>");
-            });
+            // Close the login window
+            res.set('Content-Type', 'text/html');
+            res.end("<html><body><script>setTimeout(window.close, 100);</script></body></html>");
         });
-    };
-
-    function service(svc, o) {
-        try {
-            var SVC = require('@omneedia/passport-' + svc);
-        } catch (e) {
-            util.error('passport-' + svc + ' not found.')
-        };
-        var svc = new SVC.Strategy(o, function (login, done) {
-            return done(null, login);
-        });
-        passport.use(svc);
-    };
-
-    for (var el in global.settings.auth) {
-        var o = global.settings.auth[el];
-        o.service = o.type;
-        o.redirect = 'http://127.0.0.1:' + global.manifest.server.port + '/auth/' + o.service;
-        o.host = 'http://127.0.0.1:' + global.manifest.server.port;
-        for (var el in o.login) {
-            o.login[el] = o.login[el].replace(/{CALLBACK}/g, o.redirect);
-            o.login[el] = o.login[el].replace(/{HOST}/g, o.host);
-        };
-        service(o.type, o.login);
-    };
-
-    app.get('/auth/:service', function (req, res, next) {
-        passport.authenticate(req.params.service)(req, res, next)
-    }, onAuthenticate);
-
-    passport.serializeUser(function (user, done) {
-        done(null, user);
     });
 
-    passport.deserializeUser(function (id, cb) {
-        cb(null, id);
+    authom.on("error", function (req, res, data) {
+        // called when an error occurs during authentication
+        console.log('-- ERROR ------');
+        console.log(data);
     });
 
-    app.post('/account', function (req, res) {
-        if (!req.session.user) return res.status(400).end('{"err":"NOT_AUTHENTICATED"}');
-        req.user = req.session.user;
-        var response = [];
-        global.request({
-            uri: global.Config.host + '/profile',
-            method: "POST",
-            form: {
-                task: global.Config.task,
-                user: req.user.mail.toLowerCase()
-            }
-        }, function (e, r, bx) {
-            var profiler = JSON.parse(bx);
-            for (var i = 0; i < profiler.length; i++) {
-                response.push(profiler[i].name);
-            };
-            req.user.profiles = response;
-            res.end(JSON.stringify(req.user));
-        });
+    app.get("/auth/:service", authom.app);
 
-    });
-
-    app.get('/logout', function (req, res) {
-        //console.log(req.session);
-        var authType = req.session.authType;
-        req.session.destroy();
-        if (global.settings.auth[authType.toLowerCase()]) var url = global.settings.auth[authType.toLowerCase()].logout;
-        else var url = "/bye";
-        return res.redirect(url);
-    });
-
-    app.get('/bye', function (req, res) {
-        res.setHeader('content-type', 'text/html');
-        res.end('<script>top.location.reload(true);window.close();</script>');
-    });
 }
