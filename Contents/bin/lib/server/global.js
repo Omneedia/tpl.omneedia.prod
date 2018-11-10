dc88bd319c4d1a83c5c777840f95d7bfc6047ecf
Stéphane Zucatti <stephane.zucatti@me.com>
2018-11-10 20:11:31 +0100

    small fixes regarding upload

--- Contents/bin/lib/server/global.js
+++ Contents/bin/lib/server/global.js
@@ -33,6 +33,7 @@ module.exports = function (_App) {
         "vm",
         "zlib"
     ];
+
     _App.file = {
         writer: function (ff, cb) {
             var me = this;
@@ -97,73 +98,53 @@ module.exports = function (_App) {
                 cb(results);
             });
         },
-        reader: function (ff, options, cb) {
-            var fs = require('fs');
+        reader: function (ff, cb) {
+            var isUpload = false;
             if (!cb) {
                 cb = options;
                 options = {};
             };
-            // If it's a string, the file is in the upload queue
-            // If it's an object, the file is in the cloud
-            if (cb.end) {
-                // via system
-                if (!ff) return cb.status(400).end("METHOD_NOT_ALLOWED", null);
-                if (!global.settings['docs']) return cb.status(400).end("DOCS_SETTINGS_REQUIRED", null);
-                if (!isObject(ff)) {
-                    ff = {
-                        docId: ff
-                    }
-                };
-                // Check if the file is in the upload
-                var mongoose = require('mongoose');
-                var Grid = require('gridfs-stream');
-                Grid.mongo = mongoose.mongo;
-                var conn = mongoose.createConnection(Config.session + 'upload');
-                conn.once('open', function () {
-                    var gfs = Grid(conn.db);
-                    gfs.files.find({
-                        filename: ff.docId
-                    }).toArray(function (err, files) {
-                        if (err) return cb.status(404).end('NOT_FOUND');
-                        if (files.length > 0) {
-                            var readstream = gfs.createReadStream({
-                                filename: ff.docId
-                            });
-                            readstream.on('error', function (err) {
-                                return cb.status(404).end('NOT_FOUND');
-                            });
-                            cb.set('Content-disposition', 'inline; filename="' + ff.docId + '"');
-                            cb.set("Content-Type", files[0].contentType);
-                            cb.set("Content-Length", files[0].length);
-                            readstream.pipe(cb);
-                        } else {
-                            // The file is not in the upload
-                            var set = global.settings['docs'][0];
-                            var db = set.split('://')[0];
-                            var tb = set.split('://')[1];
-                            _App.using('db').query(db, 'select * from ' + tb + ' where docId="' + ff.docId + '"', function (e, r) {
-                                if (r.length == 0) return cb.status(404).end('NOT_FOUND');
-                                cb.set('Content-disposition', 'inline; filename="' + r[0].filename + '"');
-                                cb.set("Content-Type", r[0].type);
-                                cb.set("Content-Length", r[0].size);
-                                var buf = new Buffer(r[0]._blob.split(';base64,')[1], 'base64');
-                                cb.end(buf);
-                            });
-                        }
-                    });
-                });
+            if (!cb) return false;
 
+            function err(x, code) {
+                if (!code) code = 400;
+                if (cb.end) return cb.status(code).end(x);
+                else return cb(x);
+            };
+            if (!ff) return err('METHOD_NOT_ALLOWED');
+            if (!isObject(ff)) {
+                ff = {
+                    docId: ff
+                }
             } else {
-                // via api
+                if (!ff.docId) ff.docId = ff.id;
+                if (ff.encoding) isUpload = true;
+                else isUpload = false;
+            };
+            if (!isUpload) {
                 if (!global.settings['docs']) return cb("DOCS_SETTINGS_REQUIRED", null);
-                if (!isObject(ff)) return cb("MISMATCHED_OBJECT", null);
-                if (!isObject(ff)) {
-                    ff = {
-                        docId: ff
+                var set = global.settings['docs'][0];
+                var db = set.split('://')[0];
+                var tb = set.split('://')[1];
+                _App.using('db').query(db, 'select * from ' + tb + ' where docId="' + ff.docId + '"', function (e, r) {
+                    if (cb.end) {
+                        if (r.length == 0) return cb.status(404).end('NOT_FOUND');
+                        cb.set('Content-disposition', 'inline; filename="' + r[0].filename + '"');
+                        cb.set("Content-Type", r[0].type);
+                        cb.set("Content-Length", r[0].size);
+                        var buf = new Buffer(r[0]._blob.split(';base64,')[1], 'base64');
+                        return cb.end(buf);
+                    } else {
+                        if (r.length == 0) return cb('NOT_FOUND');
+                        r[0].docId = ff.docId;
+                        if (options.output == "buffer") {
+                            r[0].buffer = new Buffer(r[0]._blob, 'base64');
+                            delete r[0]._blob;
+                        };
+                        cb(null, r[0]);
                     }
-                };
-
-                // Check if the file is in the upload
+                });
+            } else {
                 var mongoose = require('mongoose');
                 var Grid = require('gridfs-stream');
                 Grid.mongo = mongoose.mongo;
@@ -171,12 +152,12 @@ module.exports = function (_App) {
                 conn.once('open', function () {
                     var gfs = Grid(conn.db);
                     gfs.files.find({
-                        filename: ff.docId
+                        _id: ff.docId
                     }).toArray(function (err, files) {
-                        if (err) return cb.status(404).end('NOT_FOUND');
+                        if (err) return err('NOT_FOUND', 404);
                         if (files.length > 0) {
                             var readstream = gfs.createReadStream({
-                                filename: ff.docId
+                                _id: ff.docId
                             });
                             readstream.on('error', function (err) {
                                 return cb('NOT_FOUND', null);
@@ -187,31 +168,42 @@ module.exports = function (_App) {
                             });
                             readstream.on('end', function () {
                                 const fbuf = Buffer.concat(bufs);
-                                var response = {
-                                    filename: files[0].filename,
-                                    type: files[0].contentType,
-                                    size: files[0].length,
-                                    _blob: "data:" + files[0].contentType + ";base64," + fbuf.toString('base64')
+                                if (options.output.indexOf("buf") > -1) {
+                                    if (cb.end) {
+                                        cb.set('Content-disposition', 'inline; filename="' + files[0].filename + '"');
+                                        cb.set("Content-Type", files[0].contentType);
+                                        cb.set("Content-Length", files[0].length);
+                                        return cb.end(fbuf);
+                                    } else return cb(null, {
+                                        filename: files[0].filename,
+                                        originalname: ff.originalname,
+                                        type: files[0].contentType,
+                                        size: files[0].length,
+                                        buffer: fbuf
+                                    });
                                 };
-                                cb(null, response);
+                                if (cb.end) {
+                                    cb.set('Content-disposition', 'inline; filename="' + files[0].filename + '"');
+                                    cb.set("Content-Type", files[0].contentType);
+                                    cb.set("Content-Length", files[0].length);
+                                    return cb.end(fbuf);
+                                } else {
+                                    var response = {
+                                        filename: files[0].filename,
+                                        originalname: ff.originalname,
+                                        type: files[0].contentType,
+                                        size: files[0].length,
+                                        _blob: "data:" + files[0].contentType + ";base64," + fbuf.toString('base64')
+                                    };
+                                    cb(null, response);
+                                }
                             });
-                        } else {
-                            // The file is not in the upload
-                            var set = global.settings['docs'][0];
-                            var db = set.split('://')[0];
-                            var tb = set.split('://')[1];
-                            _App.using('db').query(db, 'select * from ' + tb + ' where docId="' + ff.docId + '"', function (e, r) {
-                                if (r.length == 0) return cb('NOT_FOUND', null);
-                                r[0].docId = ff.docId;
-                                cb(null, r[0]);
-                            });
-                        }
+                        } else return err('NOT_FOUND', 404);
                     });
                 });
-
-            };
+            }
         }
-    };
+    }
 
     _App.tmpdir = function (filename) {
         var OS = require('os');
